a2d1c64213284ee0062e868cd1114678a922eba9
Dhananjay Kuber <dhananjay.kuber@rtcamp.com>
2025-07-26 23:44:07 +0530

    feat: Update edit component to add Inspector control components

--- src/image-hotspot/edit.js
+++ src/image-hotspot/edit.js
@@ -2,17 +2,51 @@
  * WordPress dependencies
  */
 import { __ } from "@wordpress/i18n";
-import { useBlockProps } from "@wordpress/block-editor";
+import { useBlockProps, InspectorControls } from "@wordpress/block-editor";
+import { TextControl, PanelBody } from "@wordpress/components";
+import { Fragment } from "@wordpress/element";
 
-// Styles
+/**
+ * Internal dependencies
+ */
+import MediaUploadComponent from "../components/media-upload";
+import HotspotList from "../components/hotspot-list";
+
+/**
+ * Styles
+ */
 import "./editor.scss";
 
-export default function Edit() {
+export default function Edit({ attributes, setAttributes }) {
+	const { image, imageDescription, hotspots } = attributes;
+
 	const blockProps = useBlockProps();
 
 	return (
-		<p {...blockProps}>
-			{__("Image Hotspot - hello from the editor!", "image-hotspot")}
-		</p>
+		<Fragment>
+			<InspectorControls>
+				<PanelBody title={__("Image", "image-hotspot")} initialOpen={true}>
+					<MediaUploadComponent
+						onSelect={() => {}}
+						onRemove={() => {}}
+						value={image}
+					/>
+					<TextControl
+						label={__("Image Description", "image-hotspot")}
+						value={imageDescription}
+						onChange={(value) => setAttributes({ imageDescription: value })}
+						help={__("Description for screen readers.", "image-hotspot")}
+					/>
+				</PanelBody>
+
+				<PanelBody title={__("Hotspots", "image-hotspot")} initialOpen={true}>
+					<HotspotList hotspots={hotspots} setAttributes={setAttributes} />
+				</PanelBody>
+			</InspectorControls>
+
+			<div {...blockProps}>
+				{__("Image Hotspot - hello from the editor!", "image-hotspot")}
+			</div>
+		</Fragment>
 	);
 }
